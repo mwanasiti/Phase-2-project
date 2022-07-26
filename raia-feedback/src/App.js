cc700121f045ea95e75066318c6a158ae2e56f80
Mwanasiti <ali.mariam.mwanasiti@gmail.com>
2022-07-26 22:03:04 +0300

    yui

--- raia-feedback/src/App.js
+++ raia-feedback/src/App.js
@@ -1,5 +1,5 @@
 import {React, useState, useEffect} from 'react'
-import Form from './Form';
+// import Form from './src/Form'
 
 const App = () => {
   const [users, setUsers] = useState([]);
---
+++ raia-feedback/src/components/Form.js
@@ -0,0 +1,15 @@
+// import Form from './Form';
+
+// const App = () => {
+//   return (
+//     <div className='App'>
+//       <div className='container'>
+//         <h1 className='text-center'>Form</h1>
+//         <Form />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
+
